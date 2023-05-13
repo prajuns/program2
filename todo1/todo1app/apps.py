993687e4793eb1cf8f7651d825413a533ec43f11
prajuns <130234119+prajuns@users.noreply.github.com>
2023-05-14 00:18:30 +0530

    Delete todo1 directory
    ok

--- todo1/todo1app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Todo1AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todo1app'
--- todo1/todo1app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from . models import todo1
-
-class todo1form(forms.ModelForm):
-    class Meta:
-        model=todo1
-        fields=['name','priority','date']
